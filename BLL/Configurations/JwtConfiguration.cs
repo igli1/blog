db77152e356d77def99597c3b28ef4e01e81e687
iglij <igli.janko@hotmail.com>
2025-01-02 16:44:28 +0100

    Update appsettings.json add jwt configuration

---
+++ BLL/Configurations/JwtConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace BLL.Configurations;
+
+public class JwtConfiguration
+{
+    public static string SectionName = "JwtConfiguration";
+    public string Key { get; set; }
+    public int TokenValidityInMinutes { get; set; }
+    public int RefreshTokenValidityInDays { get; set; }
+}
