529e83fb9d9898d303433736dbb10d2c0e514e68
iglij <igli.janko@hotmail.com>
2025-01-03 22:27:54 +0100

    Validate Post Status

---
+++ WebAPI/Helpers/ValidPostStatusAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using DAL.Enums;
+
+namespace WebAPI.Helpers;
+
+public class ValidPostStatusAttribute : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        if (value is PostStatus status)
+        {
+            if (!Enum.IsDefined(typeof(PostStatus), status))
+            {
+                return new ValidationResult($"Invalid post status: {status}. Valid values are: {string.Join(", ", Enum.GetNames(typeof(PostStatus)))}");
+            }
+            return ValidationResult.Success;
+        }
+
+        return new ValidationResult("Post status is required.");
+    }
+}
